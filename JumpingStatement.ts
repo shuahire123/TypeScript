17dbe069f74eb9cf382ea92c421977411a5592f5
shubham <shuahire123@gmail.com>
2026-01-11 20:05:29 +0530

    new code

---
+++ JumpingStatement.ts
@@ -0,0 +1,25 @@
+//break
+for(let i:number=0;i<=10;i++)
+{
+    if(i==7)
+    {
+        break;
+    }
+    else{
+        console.log(i);
+    }
+}
+console.log("**************************")
+let i:number=1;
+while(i<=10)
+{
+    if(i==7)
+    {   i++;
+        continue;
+    }
+    else
+    {
+        console.log(i);
+    }
+    i++;
+}
---
+++ SwitchCase.ts
@@ -0,0 +1,16 @@
+let browser:String="chroMe";
+switch(browser.toLowerCase())
+{
+    case "chrome":
+        console.log("launch chrome");
+        break;
+
+    case "firefox":
+        console.log("launch firefox");
+        break;
+    case "edge":
+        console.log("launch edge");
+        break;
+    default:
+        console.log("please enter valid browser name");
+}
